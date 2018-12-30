829cfe814f4318a7d1915e2f885c60e65fbcd505
strangegt <34459597+strangegt@users.noreply.github.com>
2018-12-30 03:12:39 +0100

    Update jquery.unobtrusive-ajax.js

--- src/jquery.unobtrusive-ajax.js
+++ src/jquery.unobtrusive-ajax.js
@@ -197,7 +197,7 @@
             return;
         }
         asyncRequest(this, {
-            url: this.action,
+            url: clickTarget && clickTarget.attr('formaction') || this.action,
             type: this.method || "GET",
             data: clickInfo.concat($(this).serializeArray())
         });
